2bb2387a173fa7174b5b78ba7cc593d52e7afa1a
Matt Graver <mattgr34dev@gmail.com>
2023-05-03 18:54:08 +0700

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,10 @@
+RegisterCommand('startrecord', function()
+    StartRecording(1)
+end, false)
+
+RegisterCommand('stoprecord', function()
+    StopRecordingAndSaveClip()
+end, false)
+
+RegisterKeyMapping('stoprecord', 'Stop Record', 'keyboard', 'F6')
+RegisterKeyMapping('startrecord', 'Start Record', 'keyboard', 'F5')
---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'MattGr34dev <mattgr34dev@gmail.com>'
+description 'Rockstar Editor recording'
+version '1.0.0'
+
+client_script 'client.lua'
